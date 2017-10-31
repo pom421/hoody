924944fc38c197f3c6a94adf6c1c2caf2e7a93b1
PO Mauguet <mauguet.po@gmail.com>
2017-10-31 23:15:33 +0100

    promesses promesses

--- index.js
+++ index.js
@@ -12,22 +12,21 @@
 *----------------------------------------------------------------------------------------*/
 
 const express = require("express")
-const fs = require("fs")
-const fsExtra = require("fs-extra")
+const fs = require("fs-extra")
 const inquirer = require("inquirer")
 
-
 const app = express()
-const args = process.argv.slice(2)
+const argv = process.argv.slice(2)
 
 const LOCAL_USER_PATH = process.env.PWD
 const PREF_FILE = process.env.PWD + "/config.json"
 const MSG_ERR = "Appel incorrect de Hoody"
 
+const EXIT_BAD_SYNTAX = 1
 const EXIT_WWW_NOT_FOUND = 2
 
 // objet par défaut pour ssh-deployer
-const PREF_DEFAULT = {
+const SSH_DEPLOYER_DEFAULT = {
   localPath: process.env.PWD,
   currentReleaseLink: "htdocs",
   exclude: [".svn/**", ".git/**"]
@@ -38,13 +37,13 @@ const PREF_DEFAULT = {
 main()
 
 function main() {
-  if (!args[0]) {
+  if (!argv[0]) {
     console.log(MSG_ERR)
     help()
-    process.exit(1)
+    process.exit(EXIT_BAD_SYNTAX)
   }
 
-  switch (args[0].toUpperCase()) {
+  switch (argv[0].toUpperCase()) {
     case "INIT":
       init()
       break
@@ -57,7 +56,7 @@ function main() {
     default:
       console.log(MSG_ERR)
       help()
-      process.exit(1)
+      process.exit(EXIT_BAD_SYNTAX)
   }
 }
 
@@ -71,33 +70,29 @@ function help() {
 
 function init() {
   console.log("dans init")
-  // si fichier config.json inexistant, création en posant des questions
-  fs.stat(PREF_FILE, (err, stats) => {
+  fs.stat(PREF_FILE)
+    .then(() => {
+      console.log(`Fichier des préférences ${PREF_FILE} trouvé`)
+      console.log("Faire un hoody run ou hoody deploy pour continuer")
 
-    if (err) {
+    })
+    .catch(err => {
+      // si fichier config.json inexistant, création en posant des questions
       console.log(`Le fichier ${PREF_FILE} n'existe pas`)
 
-      askPreferences(copyTemplate)
+      askPreferences()
+        .then(() => {
+          // copie du fichier index.html et index.md du répertoire template dans le répertoire courant
+          copyTemplate()
+        })
 
-    } else {
-      console.log(`Fichier des préférences ${PREF_FILE} trouvé`)
-
-      const data = require(PREF_FILE)
-
-      console.log("\nInfos : copie vers /venezia/htdocs/" + data.project)
-
-      copyTemplate()
-
-      console.log("Faire un hoody run ou hoody deploy pour continuer")
-    }
-  })
-  // copie du fichier index.html et index.md du répertoire template dans le répertoire courant
+    })
 }
 
-function askPreferences(callback) {
+function askPreferences() {
   console.log("\n")
 
-  inquirer.prompt([{
+  return inquirer.prompt([{
     name: "host",
     message: "Adresse du serveur (nom DNS ou adresse IP)",
     default: "venezia.appli.dgfip",
@@ -163,61 +158,50 @@ function askPreferences(callback) {
     }
   }]).then(answers => {
 
-    const answersCompleted = Object.assign({}, PREF_DEFAULT, answers)
-
-    try {
-      fs.writeFileSync(PREF_FILE, JSON.stringify(answersCompleted, null, "\t"))
-    } catch (err) {
-
-      console.error(`\nErreur pour écrire le fichier ${PREF_FILE}. \nVeuillez vérifier les permissions sur le fichier.`)
-      //console.error(JSON.stringify(err, null, "\t"))
-    }
-
-    callback()
-
+    const answersCompleted = Object.assign({}, SSH_DEPLOYER_DEFAULT, answers)
+    fs.writeFileSync(PREF_FILE, JSON.stringify(answersCompleted, null, "\t"))
+  }).catch(err => {
+    console.error(`\nErreur pour écrire le fichier ${PREF_FILE}. \nVeuillez vérifier les permissions sur le fichier.`)
   })
 }
 
 function copyTemplate() {
 
-  fsExtra.stat(LOCAL_USER_PATH + "/www", (err, stats) => {
-    if (err) {
-      console.log("Le répertoire www n'existe pas")
-        fsExtra.mkdir(LOCAL_USER_PATH + "/www", err => {
-          if (err) { 
-            console.error("Erreur lors de la copie du répertoire template", err)
-          } else {
-            fsExtra.copy(__dirname + "/template/", LOCAL_USER_PATH + "/www/", err => {
-              if (err) {
-                console.error("Erreur lors de la copie du répertoire template", err)
-              } else {
-                console.log("Répertoire template copié")
-              }
-            })
-  
-          }
-        })
-    } else {
+  fs.stat(LOCAL_USER_PATH + "/www")
+    .then(() => {
       console.log("Le répertoire www existe déjà")
-    }
-  })
-
+    })
+    .catch(err => {
+      console.log("Le répertoire www n'existe pas")
+      fs.mkdir(LOCAL_USER_PATH + "/www")
+        .then(() => {
+          console.log("Répertoire www créé")
+          return fs.copy(__dirname + "/template/", LOCAL_USER_PATH + "/www/")
+        })
+        .then(() => {
+          console.log("Répertoire template copié")
+        })
+        .catch(err => {
+          console.error("Erreur lors de la copie du répertoire template", err)
+        })
+    })
 }
 
 function run() {
 
-  fs.stat(process.env.PWD + "/www/", err => {
-    if (err) {
+  fs.stat(process.env.PWD + "/www/")
+    .then(() => {
+      app.use(express.static(process.env.PWD + "/www"))
+
+      app.listen(8080, () => {
+        console.log("serveur lancé sur http://localhost:8080")
+      })
+    })
+    .catch(err => {
       console.error("Le répertoire www n'existe pas dans le répetoire courant. \n Veuillez lancer hoody init auparavant")
       process.exit(EXIT_WWW_NOT_FOUND)
-    }
-    app.use(express.static(process.env.PWD + "/www"))
-  
-    app.listen(8080, () => {
-      console.log("serveur lancé sur http://localhost:8080")
     })
 
-  })
 
 }
 
